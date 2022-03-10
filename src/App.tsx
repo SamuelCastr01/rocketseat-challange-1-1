b74dcedebae36bc09d7e4ab6eefc310b5e095c16
SamuelCastr0 <samueljoserodrigues13@hotmail.com>
2022-03-10 12:28:22 -0300

    challenge finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { TaskList } from './components/TaskList'
-import { Header } from "./components/Header";
-import './styles/global.scss'
-
+import { TaskList } from './components/TaskList';
+import { Header } from './components/Header';
+import './styles/global.scss';
+import { useEffect, useState, useRef } from 'react';
 
 export function App() {
+  const [count, setCount] = useState<number>(0);
+  const value = useRef(count);
+  useEffect(() => {
+    console.log('hello');
+  }, [value]);
+
   return (
     <>
-      <Header />
-      <TaskList />
+      <button onClick={() => (value.current += 1)}>
+        Contar {value.current}
+      </button>
+      <button onClick={() => setCount(count + 1)}>Contar {count}</button>
+      {/* <Header />
+      <TaskList /> */}
     </>
-  )
+  );
 }
